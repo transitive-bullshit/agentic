1ec65e4005ae9b5340b82ff7cba2b9ce91430d60
Kenneth Lane Thompson <feynmanmath@gmail.com>
2023-02-03 18:00:58 +0800

    Change the model from 'text-davinci-002-render-next' to 'text-davinci-002-render'.

--- src/chatgpt-api-browser.ts
+++ src/chatgpt-api-browser.ts
@@ -466,7 +466,7 @@ export class ChatGPTAPIBrowser extends AChatGPTAPI {
       ],
       model: this._isProAccount
         ? 'text-davinci-002-render-paid'
-        : 'text-davinci-002-render-next',
+        : 'text-davinci-002-render',
       parent_message_id: parentMessageId
     }
 
