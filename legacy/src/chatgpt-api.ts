338ac63943e600b3c03de8b920fef76f3d7d5b61
Travis Fischer <fisch0920@gmail.com>
2023-02-02 23:33:57 -0600

    feat: update chatgpt model

--- legacy/src/chatgpt-api.ts
+++ legacy/src/chatgpt-api.ts
@@ -10,7 +10,7 @@ import { fetchSSE } from './fetch-sse'
 
 // NOTE: this is not a public model, but it was leaked by the ChatGPT webapp.
 // const CHATGPT_MODEL = 'text-chat-davinci-002-20230126'
-const CHATGPT_MODEL = 'text-davinci-003'
+const CHATGPT_MODEL = 'text-chat-davinci-002-20221122'
 
 const USER_LABEL_DEFAULT = 'User'
 const ASSISTANT_LABEL_DEFAULT = 'ChatGPT'
