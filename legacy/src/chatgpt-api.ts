48edfd05e044dd1785eecbbd8df99b578cceeeb7
Travis Fischer <fisch0920@gmail.com>
2023-02-28 20:44:48 -0600

    fix: tokenizer special tokens

--- legacy/src/chatgpt-api.ts
+++ legacy/src/chatgpt-api.ts
@@ -434,10 +434,11 @@ Current date: ${currentDate}${this._sepToken}\n\n`
     if (this._isChatGPTModel) {
       // With this model, "<|im_end|>" is 1 token, but tokenizers aren't aware of it yet.
       // Replace it with "<|endoftext|>" (which it does know about) so that the tokenizer can count it as 1 token.
-      text = text.replace(/<\|im_end\|>/g, '<|endoftext|>')
-      text = text.replace(/<\|im_sep\|>/g, '<|endoftext|>')
+      // text = text.replace(/<\|im_end\|>/g, '<|endoftext|>')
+      // text = text.replace(/<\|im_sep\|>/g, '<|endoftext|>')
     }
 
+    text = text.replace(/<\|endoftext\|>/g, '')
     return tokenizer.encode(text).length
   }
 
