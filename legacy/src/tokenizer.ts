95176fa0f2af7bd1b13733cad254665c11c0d3fd
Travis Fischer <fisch0920@gmail.com>
2023-03-02 17:13:21 -0600

    feat: switch tokenizer to use cl100k_base encoding for gpt-3.5-turbo model

--- legacy/src/tokenizer.ts
+++ legacy/src/tokenizer.ts
@@ -1,7 +1,7 @@
-import { encoding_for_model } from '@dqbd/tiktoken'
+import { get_encoding } from '@dqbd/tiktoken'
 
 // TODO: make this configurable
-const tokenizer = encoding_for_model('text-davinci-003')
+const tokenizer = get_encoding('cl100k_base')
 
 export function encode(input: string): Uint32Array {
   return tokenizer.encode(input)
