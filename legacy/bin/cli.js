e7feabb4d1eaf41031079e2fbf433ab4012f3ded
Travis Fischer <fisch0920@gmail.com>
2023-03-06 01:06:17 -0600

    fix: cli apiKey

--- legacy/bin/cli.js
+++ legacy/bin/cli.js
@@ -30,8 +30,11 @@ async function main() {
       default: true
     })
     .option('-t, --timeout', 'Timeout in milliseconds')
-    .option('-k, --apiKey [apiKey]', 'OpenAI API key')
-    .option('-n, --conversationName', 'Unique name for the conversation')
+    .option('-k, --apiKey <apiKey>', 'OpenAI API key')
+    .option(
+      '-n, --conversationName <conversationName>',
+      'Unique name for the conversation'
+    )
     .action(async (prompt, options) => {
       const apiKey = options.apiKey || process.env.OPENAI_API_KEY
       if (!apiKey) {
