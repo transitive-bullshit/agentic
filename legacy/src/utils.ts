79c9d4424f96a55aee5378d190da593248ebf127
Philipp Burckhardt <pburckhardt@outlook.com>
2023-06-06 21:03:39 -0400

    feat: add sleep util

--- legacy/src/utils.ts
+++ legacy/src/utils.ts
@@ -3,3 +3,6 @@ export const extractJSONObjectFromString = (text: string): string | undefined =>
 
 export const extractJSONArrayFromString = (text: string): string | undefined =>
   text.match(/\[(.|\n)*\]/gm)?.[0]
+
+export const sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms))
