7a95bf17eb3e2cbb15ed4fa9fcc3e6c7e14bbe21
Philipp Burckhardt <pburckhardt@outlook.com>
2023-06-09 14:26:37 -0400

    feat: add Twilio Conversation service

--- legacy/src/constants.ts
+++ legacy/src/constants.ts
@@ -1,2 +1,3 @@
 export const defaultOpenAIModel = 'gpt-3.5-turbo'
 export const defaultAnthropicModel = 'claude-instant-v1'
+export const BOT_NAME = 'Agentic Bot'
---
+++ legacy/test/twilio-conversation.test.ts
@@ -0,0 +1,103 @@
+import test from 'ava'
+
+import { TwilioConversationClient } from '@/services/twilio-conversation'
+
+import './_utils'
+
+test('TwilioConversationClient.createConversation', async (t) => {
+  if (!process.env.TWILIO_ACCOUNT_SID || !process.env.TWILIO_AUTH_TOKEN) {
+    return t.pass()
+  }
+  const client = new TwilioConversationClient()
+
+  const friendlyName = 'create-conversation-test'
+  const conversation = await client.createConversation(friendlyName)
+  t.is(conversation.friendly_name, friendlyName)
+
+  client.deleteConversation(conversation.sid)
+})
+
+test('TwilioConversationClient.addParticipant', async (t) => {
+  if (
+    !process.env.TWILIO_ACCOUNT_SID ||
+    !process.env.TWILIO_AUTH_TOKEN ||
+    !process.env.TWILIO_TEST_PHONE_NUMBER
+  ) {
+    return t.pass()
+  }
+  const client = new TwilioConversationClient()
+
+  const { sid: conversationSid } = await client.createConversation(
+    'add-participant-test'
+  )
+  const { sid: participantSid } = await client.addParticipant({
+    conversationSid,
+    recipientPhoneNumber: process.env.TWILIO_TEST_PHONE_NUMBER
+  })
+  t.is(participantSid.startsWith('MB'), true)
+
+  await client.deleteConversation(conversationSid)
+})
+
+test('TwilioConversationClient.sendMessage', async (t) => {
+  if (!process.env.TWILIO_ACCOUNT_SID || !process.env.TWILIO_AUTH_TOKEN) {
+    return t.pass()
+  }
+  const client = new TwilioConversationClient()
+
+  const text = 'Hello, world!'
+  const { sid: conversationSid } = await client.createConversation(
+    'send-message-test'
+  )
+  const message = await client.sendMessage({ conversationSid, text })
+  t.is(message.body, text)
+
+  await client.deleteConversation(conversationSid)
+})
+
+test('TwilioConversationClient.fetchMessages', async (t) => {
+  if (!process.env.TWILIO_ACCOUNT_SID || !process.env.TWILIO_AUTH_TOKEN) {
+    return t.pass()
+  }
+  const client = new TwilioConversationClient()
+
+  const { sid: conversationSid } = await client.createConversation(
+    'fetch-messages-test'
+  )
+  const { messages, meta } = await client.fetchMessages(conversationSid)
+  t.true(Array.isArray(messages))
+  t.is(meta.page, 0)
+
+  await client.deleteConversation(conversationSid)
+})
+
+test('TwilioConversationClient.sendAndWaitForReply', async (t) => {
+  if (
+    !process.env.TWILIO_ACCOUNT_SID ||
+    !process.env.TWILIO_AUTH_TOKEN ||
+    !process.env.TWILIO_TEST_PHONE_NUMBER
+  ) {
+    return t.pass()
+  }
+
+  t.timeout(2 * 60 * 1000)
+  const client = new TwilioConversationClient()
+
+  await t.throwsAsync(
+    async () => {
+      await client.sendAndWaitForReply({
+        recipientPhoneNumber: process.env.TWILIO_TEST_PHONE_NUMBER as string,
+        text: 'Please confirm by replying with "yes" or "no".',
+        name: 'wait-for-reply-test',
+        validate: (message) =>
+          ['yes', 'no'].includes(message.body.toLowerCase()),
+        timeoutMs: 10000, // 10 seconds
+        intervalMs: 5000 // 5 seconds
+      })
+    },
+    {
+      instanceOf: Error,
+      message: 'Reached timeout waiting for reply'
+    }
+  )
+})
