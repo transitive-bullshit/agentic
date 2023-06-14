8e6c65a4e5fb61c6b9e62d6aa52c806b1280bc84
Philipp Burckhardt <pburckhardt@outlook.com>
2023-06-14 15:55:19 -0400

    docs: add new feedback example

---
+++ legacy/scratch/examples/human-feedback-cli.ts
@@ -0,0 +1,39 @@
+import 'dotenv/config'
+import { OpenAIClient } from 'openai-fetch'
+import { z } from 'zod'
+
+import { HumanFeedbackMechanismCLI } from '@/human-feedback'
+import { Agentic, withHumanFeedback } from '@/index'
+
+async function main() {
+  const openai = new OpenAIClient({ apiKey: process.env.OPENAI_API_KEY! })
+  const ai = new Agentic({ openai })
+
+  const topicFacts = ai
+    .gpt3(`Tell me {{num}} jokes about {{topic}}`)
+    .input(
+      z.object({
+        topic: z.string(),
+        num: z.number().int().default(5).optional()
+      })
+    )
+    .output(z.array(z.string()))
+    .modelParams({ temperature: 0.9 })
+
+  const topicFactsFeedback = withHumanFeedback(topicFacts, {
+    type: 'selectN',
+    annotations: false,
+    bail: false,
+    editing: true,
+    mechanism: HumanFeedbackMechanismCLI
+  })
+
+  const out = await topicFactsFeedback.callWithMetadata({
+    topic: 'politicians',
+    num: 5
+  })
+  const feedback = out.metadata.feedback
+  console.log(JSON.stringify(feedback, null, 2))
+}
+
+main()
