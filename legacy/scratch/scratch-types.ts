6b38a7414edbc4e57ca5e6913e50d7aa444c2e36
Travis Fischer <fisch0920@gmail.com>
2023-06-14 15:18:54 -0700

    Update scratch/scratch-types.ts
    Co-authored-by: Philipp Burckhardt <pburckhardt@outlook.com>

--- legacy/scratch/scratch-types.ts
+++ legacy/scratch/scratch-types.ts
@@ -61,7 +61,7 @@ export interface FunctionDefinition {
   description?: string
 
   /**
-   * The parameters the functions accepts, described as a JSON Schema object. See the [guide](/docs/guides/gpt/function-calling) for examples, and the [JSON Schema reference](https://json-schema.org/understanding-json-schema/) for documentation about the format.
+   * The parameters the function accepts, described as a JSON Schema object. See the [guide](/docs/guides/gpt/function-calling) for examples, and the [JSON Schema reference](https://json-schema.org/understanding-json-schema/) for documentation about the format.
    */
   parameters?: { [key: string]: any }
 }
