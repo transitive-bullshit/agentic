d3e40768b56e8cb6061e3369bd67fd6a43ff2950
TZZack <464529342@qq.com>
2023-06-14 16:48:46 +0800

    feat: 增加3.5-0613、16k、16k-0613几个模型的请求地址适配

--- src/chatgpt-api.ts
+++ src/chatgpt-api.ts
@@ -200,7 +200,10 @@ export class ChatGPTAPI {
           'gpt-4-32k',
           'gpt-4-32k-0314',
           'gpt-3.5-turbo',
-          'gpt-3.5-turbo-0301'
+          'gpt-3.5-turbo-0301',
+          'gpt-3.5-turbo-0613',
+          'gpt-3.5-turbo-16k',
+          'gpt-3.5-turbo-16k-0613'
         ].includes(model)
         if (!isGpt) {
           url = `${this._apiBaseUrl}/completions`
