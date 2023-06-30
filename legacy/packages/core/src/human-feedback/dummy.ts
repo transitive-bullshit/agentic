7a568e964c9154e859cf070c5b0499853c316441
Philipp Burckhardt <pburckhardt@outlook.com>
2023-06-29 22:43:30 -0400

    feat: add dummy mechanism that bypasses human feedback

---
+++ legacy/packages/core/src/human-feedback/dummy.ts
@@ -0,0 +1,34 @@
+import {
+  HumanFeedbackMechanism,
+  HumanFeedbackType,
+  HumanFeedbackUserActions
+} from './feedback'
+
+export class HumanFeedbackMechanismDummy<
+  T extends HumanFeedbackType,
+  TOutput
+> extends HumanFeedbackMechanism<T, TOutput> {
+  protected async _select(
+    output: TOutput
+  ): Promise<TOutput extends any[] ? TOutput[0] : never> {
+    return output[0] as any
+  }
+
+  protected async _multiselect(
+    output: TOutput
+  ): Promise<TOutput extends any[] ? TOutput : never> {
+    return output as any
+  }
+
+  protected async _annotate(): Promise<string> {
+    return 'Default annotation'
+  }
+
+  protected async _edit(output: string): Promise<string> {
+    return output
+  }
+
+  protected async _askUser(): Promise<HumanFeedbackUserActions> {
+    return HumanFeedbackUserActions.Accept
+  }
+}
