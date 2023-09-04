e38a617bbeb2493d4569a2bd213ad674c4e58d15
LiYang <cctv1005s@gmail.com>
2023-09-04 14:25:09 +0800

    feat: common use encoding

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,9 +1,9 @@
 import { Tiktoken } from 'js-tiktoken/lite'
 import cl100k_base from 'js-tiktoken/ranks/cl100k_base'
 
-export function encode(input: string): Uint32Array {
-  // TODO: make this configurable
-  const encoding = new Tiktoken(cl100k_base)
+// TODO: make this configurable
+const encoding = new Tiktoken(cl100k_base)
 
+export function encode(input: string): Uint32Array {
   return new Uint32Array(encoding.encode(input))
 }
