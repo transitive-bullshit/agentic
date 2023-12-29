bcfd174294f92917e037ad58d17526cb9dbe34d0
LiYang <cctv1005s@gmail.com>
2023-12-29 12:03:27 +0800

    revert: tokenizer

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,8 +1,7 @@
-import { Tiktoken } from 'js-tiktoken/lite'
-import cl100k_base from 'js-tiktoken/ranks/cl100k_base'
+import { getEncoding } from 'js-tiktoken'
 
 // TODO: make this configurable
-const encoding = new Tiktoken(cl100k_base)
+const encoding = getEncoding('cl100k_base')
 
 export function encode(input: string): Uint32Array {
   return new Uint32Array(encoding.encode(input))
