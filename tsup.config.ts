b7bd3ce15d57c79a46c8ada1cd16b2d4aaba45a5
Travis Fischer <fisch0920@gmail.com>
2024-05-26 18:12:07 -0500

    🗿

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig([
   {
     entry: ['src/index.ts'],
     outDir: 'dist',
-    target: 'node22',
+    target: 'node18',
     platform: 'node',
     format: ['esm'],
     splitting: false,
