89c86af7efe46bf20592f1376424f1f82f059ea8
Travis Fischer <fisch0920@gmail.com>
2024-06-07 01:07:19 -0500

    feat: transition from chatgpt to agentic

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './chatgpt-api'
-export * from './chatgpt-unofficial-proxy-api'
-export * from './types'
--- src/tokenizer.ts
+++
@@ -1,8 +0,0 @@
-import { getEncoding } from 'js-tiktoken'
-
-// TODO: make this configurable
-const tokenizer = getEncoding('cl100k_base')
-
-export function encode(input: string): Uint32Array {
-  return new Uint32Array(tokenizer.encode(input))
-}
