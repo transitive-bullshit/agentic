c9cdcf4a0d691b17c8f10dae9e3ccaf678cb8689
Travis Fischer <fisch0920@gmail.com>
2024-07-27 01:09:48 -0700

    feat: improve types for extractObject and JinaClient

--- src/extract-object.ts
+++ src/extract-object.ts
@@ -1,8 +1,18 @@
 import type * as types from './types.js'
-import { createAIChain } from './create-ai-chain.js'
+import { type AIChainParams, createAIChain } from './create-ai-chain.js'
+
+export type ExtractObjectParams<Result extends types.AIChainResult = string> =
+  types.Simplify<
+    types.SetRequired<
+      Omit<AIChainParams<Result>, 'tools' | 'toolCallConcurrency' | 'params'>,
+      'schema'
+    > & {
+      params: types.SetRequired<Partial<types.ChatParams>, 'messages'>
+    }
+  >
 
 export function extractObject<Result extends types.AIChainResult = string>(
-  args: types.ExtractObjectParams<Result>
+  args: ExtractObjectParams<Result>
 ): Promise<Result> {
   const chain = createAIChain(args)
   return chain()
