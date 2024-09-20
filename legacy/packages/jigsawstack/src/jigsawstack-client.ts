6409eb42f15d1a68523827507fa5341ab924f51e
Narcisse <mechikaegon@gmail.com>
2024-09-20 12:59:58 +0100

    chore:integration tests for jigsawstack tools

--- legacy/packages/jigsawstack/src/jigsawstack-client.ts
+++ legacy/packages/jigsawstack/src/jigsawstack-client.ts
@@ -1,5 +1,6 @@
-import { AIFunctionsProvider, assert, getEnv } from '@agentic/core'
+import { aiFunction,AIFunctionsProvider, assert, getEnv } from '@agentic/core'
 import ky, { type KyInstance } from 'ky'
+import { z } from 'zod'
 
 export namespace jigsawstack {
   export interface BaseResponse {
@@ -154,6 +155,14 @@ export class JigsawStackClient extends AIFunctionsProvider {
     })
   }
 
+  @aiFunction({
+    name: 'jigsawstack_ai_search',
+    description:
+      'Perform web searches and retrieve high-quality results of the given query',
+    inputSchema: z.object({
+      query: z.string().describe('The search query')
+    })
+  })
   async aiSearch(params: jigsawstack.SearchParams) {
     return this.ky
       .get('web/search', {
@@ -164,6 +173,18 @@ export class JigsawStackClient extends AIFunctionsProvider {
       .json<jigsawstack.SearchResponse>()
   }
 
+  @aiFunction({
+    name: 'jigsawstack_ai_scrape',
+    description: 'Scrape any website',
+    inputSchema: z.object({
+      url: z.string().describe('The url to scrape its content'),
+      element_prompts: z
+        .array(z.string())
+        .describe(
+          'The items to scrape (retrieve) from the given url. eg. Page title, price, etc'
+        )
+    })
+  })
   async aiScrape(params: jigsawstack.ScrapeParams) {
     return this.ky
       .post('ai/scrape', {
@@ -174,6 +195,18 @@ export class JigsawStackClient extends AIFunctionsProvider {
       .json<jigsawstack.ScrapeResponse>()
   }
 
+  @aiFunction({
+    name: 'jigsawstack_vocr',
+    description:
+      'Recognise, describe and retrieve data within an image with great accuracy.',
+    inputSchema: z.object({
+      url: z.string().describe('The image url to OCR'),
+      prompt: z
+        .string()
+        .default('Describe the image in detail.')
+        .describe('What you want to know  or retrieve from the image')
+    })
+  })
   async vocr(params: jigsawstack.VOCRParams) {
     return this.ky
       .post('vocr', {
@@ -184,6 +217,18 @@ export class JigsawStackClient extends AIFunctionsProvider {
       .json<jigsawstack.VOCRResponse>()
   }
 
+  @aiFunction({
+    name: 'jigsawstack_text_to_sql',
+    description: 'Generate semantically correct SQL queries from text.',
+    inputSchema: z.object({
+      prompt: z
+        .string()
+        .describe('The text that will be translated to an SQL query.'),
+      sql_schema: z
+        .string()
+        .describe('The valid sql schema where the query will be run')
+    })
+  })
   async textToSql(
     params: jigsawstack.TextToSqlParams
   ): Promise<jigsawstack.TextToSqlResponse> {
@@ -196,6 +241,14 @@ export class JigsawStackClient extends AIFunctionsProvider {
       .json<jigsawstack.TextToSqlResponse>()
   }
 
+  @aiFunction({
+    name: 'jigsawstack_speech_to_text',
+    description:
+      'Convert audio/video files into accurate text transcriptions instantly.',
+    inputSchema: z.object({
+      url: z.string().describe('The audio or video url')
+    })
+  })
   async speechToText(
     params: jigsawstack.SpeechToTextParams
   ): Promise<jigsawstack.SpeechToTextResponse> {
