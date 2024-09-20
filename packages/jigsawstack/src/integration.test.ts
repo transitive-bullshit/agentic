24eeb798f95f1f7757722abe2ea62fba56f6e05f
Narcisse <mechikaegon@gmail.com>
2024-09-20 12:59:58 +0100

    chore:integration tests for jigsawstack tools

--- packages/jigsawstack/src/integration.test.ts
+++ packages/jigsawstack/src/integration.test.ts
@@ -1,57 +1,71 @@
 // eslint-disable-next-line simple-import-sort/imports
-
+import { openai } from '@ai-sdk/openai'
+import { generateText } from 'ai'
 import { expect, test } from 'vitest'
+import { createAISDKTools } from '../../ai-sdk'
+import { JigsawStackClient } from './jigsawstack-client'
 
-import { type jigsawstack,JigsawStackClient } from './jigsawstack-client'
+// Do ensure to set your JIGSAWSTACK_API_KEY environment variable.
 
-const jigsaw = new JigsawStackClient()
+const jigsaw = new JigsawStackClient({ timeoutMs: 30_000 })
 
-test.skip('should run successfully and return the search result', async () => {
-  const params: jigsawstack.SearchParams = {
-    query: 'The leaning tower of pisa',
-    ai_overview: true,
-    spell_check: true
-  }
-  const result = await jigsaw.aiSearch(params)
-  expect(result).toBeTruthy()
-  expect(result.success).toBe(true)
+test.skip('should run successfully and return search result', async () => {
+  const { toolResults } = await generateText({
+    model: openai('gpt-4o-mini'),
+    tools: createAISDKTools(jigsaw),
+    toolChoice: 'required',
+    prompt: 'Tell me about "Santorini"'
+  })
+
+  // console.log(toolResults, 'toolResults')
+  expect(toolResults[0]).toBeTruthy()
 })
 
-test.skip('should run successfully and return the valid sql result', async () => {
-  const params: jigsawstack.TextToSqlParams = {
-    sql_schema:
-      "CREATE TABLE Transactions (transaction_id INT PRIMARY KEY, user_id INT NOT NULL,total_amount DECIMAL(10, 2 NOT NULL, transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,status VARCHAR(20) DEFAULT 'pending',FOREIGN KEY(user_id) REFERENCES Users(user_id))",
+test.skip('should scrape url successfully and return result', async () => {
+  const { toolResults } = await generateText({
+    model: openai('gpt-4o-mini'),
+    tools: createAISDKTools(jigsaw),
+    toolChoice: 'required',
+    prompt: 'Scrape https://jigsawstack.com and tell me their title'
+  })
+
+  // console.log(toolResults[0], 'toolResults')
+  expect(toolResults[0]).toBeTruthy()
+})
+
+test.skip('should perform vision based OCR and return result', async () => {
+  const { toolResults } = await generateText({
+    model: openai('gpt-4o-mini'),
+    tools: createAISDKTools(jigsaw),
+    toolChoice: 'required',
     prompt:
-      'Generate a query to get transactions that amount exceed 10000 and sort by when created'
-  }
-
-  const result = await jigsaw.textToSql(params)
-  expect(result).toBeTruthy()
-  expect(result.success).toBe(true)
-})
-
-test.skip('should return result.success is true for successful vocr', async () => {
-  const result = await jigsaw.vocr({
-    prompt: 'Describe the image in detail',
-    url: 'https://rogilvkqloanxtvjfrkm.supabase.co/storage/v1/object/public/demo/Collabo%201080x842.jpg?t=2024-03-22T09%3A22%3A48.442Z'
+      'Tell me about this image : https://rogilvkqloanxtvjfrkm.supabase.co/storage/v1/object/public/demo/Collabo%201080x842.jpg?t=2024-03-22T09%3A22%3A48.442Z'
   })
-  expect(result.success).toBe(true)
+  // console.log(toolResults[0], 'toolResults')
+  expect(toolResults[0]).toBeTruthy()
 })
 
-test('should run successfully and return the transcribe result', async () => {
-  const data = await jigsaw.speechToText({
-    url: 'https://rogilvkqloanxtvjfrkm.supabase.co/storage/v1/object/public/demo/Video%201737458382653833217.mp4?t=2024-03-22T09%3A50%3A49.894'
+test.skip('should perform speech to text  and return result', async () => {
+  const { toolResults } = await generateText({
+    model: openai('gpt-4o-mini'),
+    tools: createAISDKTools(jigsaw),
+    toolChoice: 'required',
+    prompt:
+      'Get the transcription from this video url : https://rogilvkqloanxtvjfrkm.supabase.co/storage/v1/object/public/demo/Video%201737458382653833217.mp4?t=2024-03-22T09%3A50%3A49.894Z'
   })
-  expect(data).toBeTruthy()
-  expect(data.success).toBe(true)
+  // console.log(toolResults[0], 'toolResults')
+  expect(toolResults[0]).toBeTruthy()
 })
 
-test.skip('should run successfully and return scrape result', async () => {
-  const params: jigsawstack.ScrapeParams = {
-    url: 'https://jigsawstack.com/pricing',
-    element_prompts: ['Pro Plan']
-  }
-  const result = await jigsaw.aiScrape(params)
-  expect(result).toBeTruthy()
-  expect(result.success).toBe(true)
+test('should perform text to sql and return result', async () => {
+  const prompt = `
+  Generate a query to get transactions that amount exceed 10000 and sort by when created. Given this schema:
+    "CREATE TABLE Transactions (transaction_id INT PRIMARY KEY, user_id INT NOT NULL,total_amount DECIMAL(10, 2 NOT NULL, transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,status VARCHAR(20) DEFAULT 'pending',FOREIGN KEY(user_id) REFERENCES Users(user_id))"`
+  const { toolResults } = await generateText({
+    model: openai('gpt-4o-mini'),
+    tools: createAISDKTools(jigsaw),
+    toolChoice: 'required',
+    prompt
+  })
+  expect(toolResults[0]).toBeTruthy()
 })
