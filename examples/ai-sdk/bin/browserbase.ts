9a94be9b8388c1b9e3899ad1af896015f9ad3297
Travis Fischer <fisch0920@gmail.com>
2024-11-21 15:49:45 +0700

    chore: minor

--- examples/ai-sdk/bin/browserbase.ts
+++ examples/ai-sdk/bin/browserbase.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import 'dotenv/config'
 
 import { createOpenAI } from '@ai-sdk/openai'
--- examples/dexter/bin/extract-user.ts
+++ examples/dexter/bin/extract-user.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import 'dotenv/config'
 
 import { extractObject, Msg } from '@agentic/core'
