0ec220446dc5b8cc5da4c4466cd81614c218bdff
Travis Fischer <fisch0920@gmail.com>
2025-03-18 21:02:16 +0800

    fix: firecrawl updates from last pr

--- packages/firecrawl/src/index.ts
+++ packages/firecrawl/src/index.ts
@@ -1,13 +1,1 @@
-export type {
-  ErrorResponse,
-  ExtractParams,
-  ExtractResponse,
-  ExtractStatusResponse,
-  FirecrawlClientConfig,
-  GenerateLLMsTextParams,
-  GenerateLLMsTextResponse,
-  GenerateLLMsTextStatusResponse,
-  ScrapeParams,
-  ScrapeResponse
-} from './firecrawl-client.js'
-export { FirecrawlClient } from './firecrawl-client.js'
+export * from './firecrawl-client'
