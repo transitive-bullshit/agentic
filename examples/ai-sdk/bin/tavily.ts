49016911cefbb381a8496a22491966f7f4460107
Travis Fischer <fisch0920@gmail.com>
2025-04-01 21:10:31 +0700

    ⏯

--- examples/ai-sdk/bin/tavily.ts
+++ examples/ai-sdk/bin/tavily.ts
@@ -7,10 +7,13 @@ import { generateText } from 'ai'
 
 async function main() {
   const tavily = new TavilyClient()
-  const openai = createOpenAI({ compatibility: 'strict' })
+  const openai = createOpenAI({ compatibility: 'compatible' })
 
   const result = await generateText({
-    model: openai('gpt-4o-mini'),
+    model: openai('gpt-4o-mini', {
+      // TODO: https://github.com/transitive-bullshit/agentic/issues/702
+      // structuredOutputs: true
+    }),
     tools: createAISDKTools(tavily),
     toolChoice: 'required',
     temperature: 0,
