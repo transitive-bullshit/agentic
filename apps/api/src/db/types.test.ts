05ae5e307ee7900851f88670ecef766ef7eaebf8
Travis Fischer <fisch0920@gmail.com>
2025-05-14 19:41:28 +0700

    feat: remove workos; update drizzle fork

--- apps/api/src/db/types.test.ts
+++ apps/api/src/db/types.test.ts
@@ -1,14 +1,11 @@
 import { expectTypeOf, test } from 'vitest'
 
-import type { schema } from '.'
-import type { User } from './types'
+import type { RawUser, User } from './types'
 
-type UserSelect = typeof schema.users.$inferSelect
 type UserKeys = Exclude<keyof User, 'providers'>
 
 test('User types are compatible', () => {
-  // TODO: { github?: AuthProvider | undefined } !== { github: AuthProvider | undefined }
-  expectTypeOf<UserSelect>().toExtend<User>()
+  expectTypeOf<RawUser>().toExtend<User>()
 
-  expectTypeOf<User[UserKeys]>().toEqualTypeOf<UserSelect[UserKeys]>()
+  expectTypeOf<User[UserKeys]>().toEqualTypeOf<RawUser[UserKeys]>()
 })
--- apps/api/src/lib/workos.ts
+++
@@ -1,7 +0,0 @@
-import { WorkOS } from '@workos-inc/node'
-
-import { env } from './env'
-
-export const workos = new WorkOS(env.WORKOS_API_KEY, {
-  clientId: env.WORKOS_CLIENT_ID
-})
