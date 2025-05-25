6f35cffae9c169a8ff3dd00b3dac672ee6fc895b
Travis Fischer <fisch0920@gmail.com>
2025-05-26 02:51:39 +0700

    🚌

--- packages/schemas/src/define-config.ts
+++ packages/schemas/src/define-config.ts
@@ -6,6 +6,12 @@ import {
   agenticProjectConfigSchema
 } from './agentic-project-config-schema'
 
+/**
+ * This method allows Agentic projects to define their configs in a type-safe
+ * way from `agentic.config.ts` files.
+ *
+ * It parses the given input config and performs basic validation.
+ */
 export function defineConfig(
   config: AgenticProjectConfigInput
 ): AgenticProjectConfig {
