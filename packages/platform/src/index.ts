f5622da6cfe04ac4e284ca9a11eb4e8cdee8c5f1
Travis Fischer <fisch0920@gmail.com>
2025-05-30 02:57:08 +0700

    🍺

--- packages/platform/src/index.ts
+++ packages/platform/src/index.ts
@@ -1,3 +1,4 @@
 export * from './define-config'
 export * from './resolve-agentic-project-config'
 export * from './validate-agentic-project-config'
+export { defaultFreePricingPlan } from '@agentic/platform-schemas'
