8f028bf538f0cb62cdbdf9095ffb7305451942b0
Travis Fischer <fisch0920@gmail.com>
2025-06-06 03:32:34 +0700

    🔰

--- packages/fixtures/valid/basic-mcp/agentic.config.ts
+++ packages/fixtures/valid/basic-mcp/agentic.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from '@agentic/platform'
 
 export default defineConfig({
   name: 'test-basic-mcp',
-  originUrl: 'http://localhost:8080',
+  // originUrl: 'http://localhost:8080/stream',
+  originUrl: 'https://agentic-basic-mcp-test.onrender.com/stream',
   originAdapter: {
     type: 'mcp'
   }
