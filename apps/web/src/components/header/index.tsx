598eb1062b0d50b964d4068195958117981fe5a7
Travis Fischer <fisch0920@gmail.com>
2025-06-28 11:14:30 -0500

    feat: fix site header logo on mobile

--- apps/web/src/components/header/index.tsx
+++ apps/web/src/components/header/index.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useTheme } from 'next-themes'
+
 import { ActiveLink } from '@/components/active-link'
 import { useAgentic } from '@/components/agentic-provider'
 import { DarkModeToggle } from '@/components/dark-mode-toggle'
@@ -9,17 +11,26 @@ import { cn } from '@/lib/utils'
 import styles from './styles.module.css'
 
 export function Header() {
+  const { resolvedTheme } = useTheme()
   const ctx = useAgentic()
 
   return (
     <header className={cn(styles.header, 'shadow-sm')}>
       <div className={styles.headerContent}>
         <ActiveLink className={styles.logo} href='/'>
-          AGENTIC
+          <img
+            src={
+              resolvedTheme === 'dark'
+                ? '/agentic-name-flat-dark.svg'
+                : '/agentic-name-flat-light.svg'
+            }
+            alt='AGENTIC'
+            className='w-[144px]'
+          />
         </ActiveLink>
 
         <div className='flex justify-end items-center h-full gap-4'>
-          <ActiveLink href='/marketplace' className='link'>
+          <ActiveLink href='/marketplace' className='link hidden sm:block'>
             MCP Marketplace
           </ActiveLink>
 
